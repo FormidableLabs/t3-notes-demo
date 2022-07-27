aebd8e3c56c7d79ede4d331075b226b52e5e36af
Trey Hoover <treyhoover@gmail.com>
2022-07-27 14:26:44 +0200

    Draw the rest of the owl

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,9 +1,11 @@
 // src/server/trpc/router/index.ts
 import { t } from "../utils";
 import { authRouter } from "./auth";
+import { notesRouter } from "./notes";
 
 export const appRouter = t.router({
   auth: authRouter,
+  notes: notesRouter,
 });
 
 // export type definition of API
